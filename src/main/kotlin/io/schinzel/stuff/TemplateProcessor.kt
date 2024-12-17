7211f496b58985f94488f6772d48c13925f2bf6e
Henrik Schinzel <henrik@schinzel.io>
2024-12-17 15:09:18 +0100

    code

--- src/main/kotlin/io/schinzel/stuff/TemplateProcessor.kt
+++ src/main/kotlin/io/schinzel/stuff/TemplateProcessor.kt
@@ -1,8 +1,16 @@
 package io.schinzel.stuff
 
+import io.schinzel.basic_utils_kotlin.printlnWithPrefix
 import java.io.File
 
 
+/**
+ * The purpose of this class is to process a template file and replace placeholders with values.
+ * The file to be read must be located in the same package as the class that is calling this class.
+ *
+ * @param fileName The name of the file to process.
+ * @param caller The class that is calling this class. Used to find the file to read.
+ */
 class TemplateProcessor(private val fileName: String, private val caller: Any) {
     val data: MutableMap<String, String> = mutableMapOf()
     fun addData(key: String, value: String): TemplateProcessor {
@@ -11,16 +19,21 @@ class TemplateProcessor(private val fileName: String, private val caller: Any) {
     }
 
     fun getProcessedTemplate(): String {
-        val isJar = isRunningFromJar(caller)
         val fileContent = when {
-            isJar -> readFileInJar(fileName, caller)
+            isRunningFromJar(caller) -> readFileInJar(fileName, caller)
             else -> readFileInSrc(fileName, caller)
         }
         return applyData(fileContent, data)
     }
 
+
     companion object {
 
+        /**
+         * @param fileContent The content of the file to process.
+         * @param data The data to replace placeholders with.
+         * @return The file content with placeholders replaced with values from the data map.
+         */
         private fun applyData(fileContent: String, data: Map<String, String>): String {
             var content = fileContent
             data.forEach { (key, value) ->
@@ -29,7 +42,13 @@ class TemplateProcessor(private val fileName: String, private val caller: Any) {
             return content
         }
 
-
+        /**
+         * Reads a file from a jar file.
+         *
+         * @param fileName The name of the file to read.
+         * @param caller The class that is calling this class. Used to find the file to read.
+         * @return The content of the file.
+         */
         private fun readFileInJar(fileName: String, caller: Any): String {
             // For example: io/schinzel/page_elements_kotlin/page/greeting_pe
             val pathToCallerClass = getPathToCallerClass(caller)
@@ -40,6 +59,13 @@ class TemplateProcessor(private val fileName: String, private val caller: Any) {
         }
 
 
+        /**
+         *  Reads a file from the src folder.
+         *
+         * @param fileName The name of the file to read.
+         * @param caller The class that is calling this class. Used to find the file to read.
+         * @return The content of the file.
+         */
         private fun readFileInSrc(fileName: String, caller: Any): String {
             // For example: Users/schinzel/code/page-elements-kotlin
             val projectDir = File("")
@@ -70,8 +96,8 @@ class TemplateProcessor(private val fileName: String, private val caller: Any) {
          * @return True if the caller is running from a jar file.
          */
         private fun isRunningFromJar(caller: Any): Boolean {
-            val location = caller::class.java.protectionDomain.codeSource.location.toString()
-            return location.endsWith(".jar")
+            val location = this::class.java.protectionDomain.codeSource.location.toString()
+            return location.endsWith(".jar").printlnWithPrefix("Running from jar")
         }
     }
 }
