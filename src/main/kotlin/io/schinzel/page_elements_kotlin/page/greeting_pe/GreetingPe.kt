d2a195b74cea1e698f62779ffac45e1b2590d55f
Henrik Schinzel <henrik@schinzel.io>
2024-12-17 10:49:41 +0100

    code

--- src/main/kotlin/io/schinzel/page_elements_kotlin/page/greeting_pe/GreetingPe.kt
+++ src/main/kotlin/io/schinzel/page_elements_kotlin/page/greeting_pe/GreetingPe.kt
@@ -2,13 +2,10 @@ package io.schinzel.page_elements_kotlin.page.greeting_pe
 
 import io.schinzel.basic_utils_kotlin.println
 import io.schinzel.page_elements_kotlin.IPageElement
-import io.schinzel.stuff.ITemplateProcessorDeep
 import io.schinzel.stuff.TemplateProcessor
 
-class GreetingPe : IPageElement, ITemplateProcessorDeep {
-    override val data = mutableMapOf<String, String>()
+class GreetingPe : IPageElement {
     override fun getHtml(): String {
-
         return TemplateProcessor("GreetingPe.html")
             .addData("firstName", "Pelle")
             .getProcessedTemplate()
