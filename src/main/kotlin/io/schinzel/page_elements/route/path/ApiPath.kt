35b05458a574407181cb9675501226fc436f255c
Henrik Schinzel <henrik@schinzel.io>
2024-12-21 16:06:13 +0100

    code

--- src/main/kotlin/io/schinzel/page_elements/route/path/ApiPath.kt
+++ src/main/kotlin/io/schinzel/page_elements/route/path/ApiPath.kt
@@ -1,5 +1,7 @@
 package io.schinzel.page_elements.route.path
 
 class ApiPath(relativePath: String) : IPath {
+    // pathen = relativePath + klass namne till kebab case
+    // Lägg till api först i pathen
     override val path: String = relativePath
 }
