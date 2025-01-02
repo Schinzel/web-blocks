6bebd5c668191180e671b0c9ae431a2905c4ff7c
Henrik Schinzel <henrik@schinzel.io>
2025-01-02 05:52:11 +0100

    code

--- src/main/kotlin/io/schinzel/samples/web/main.kt
+++ src/main/kotlin/io/schinzel/samples/web/main.kt
@@ -4,6 +4,10 @@ import io.schinzel.web.InitWebApp
 import io.schinzel.web.WebAppConfig
 import io.schinzel.web.request_handler.log.ConsoleLogger
 
+/**
+ * The purpose of this class is to start a demo of the web framework.
+ * See README.md for links to use with this demo.
+ */
 fun main() {
     InitWebApp(
         WebAppConfig(
--- src/main/kotlin/io/schinzel/web/WebAppConfig.kt
+++ src/main/kotlin/io/schinzel/web/WebAppConfig.kt
@@ -5,11 +5,17 @@ import io.schinzel.web.request_handler.log.ILogger
 
 /**
  * The purpose of this class is to hold configuration settings for the web app.
+ * @param endpointPackage The package where the endpoints are located.
+ * @param port The port the web app will listen to. Default value is 5555.
+ * @param logger The logger to use. Default value is a console logger.
+ * @param localTimezone The timezone to use. Default value is "Europe/Stockholm".
+ * @param prettyFormatHtml If the html should be pretty formatted. Default value is true.
+ * For production environment set this should be false.
  */
 data class WebAppConfig(
     val endpointPackage: String,
     val port: Int = 5555,
-    val logger: ILogger = ConsoleLogger(true),
+    val logger: ILogger = ConsoleLogger(prettyPrint = true),
     val localTimezone: String = "Europe/Stockholm",
     val prettyFormatHtml: Boolean = true,
 )
