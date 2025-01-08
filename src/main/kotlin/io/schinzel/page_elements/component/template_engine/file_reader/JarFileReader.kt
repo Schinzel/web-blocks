abe8f769d295810d5c27c120d6132b6ee967e53a
Henrik Schinzel <henrik@schinzel.io>
2025-01-08 05:44:04 +0100

    code

--- src/main/kotlin/io/schinzel/page_elements/component/template_engine/file_reader/JarFileReader.kt
+++ src/main/kotlin/io/schinzel/page_elements/component/template_engine/file_reader/JarFileReader.kt
@@ -4,24 +4,20 @@ package io.schinzel.page_elements.component.template_engine.file_reader
 /**
  * The purpose of this class is to read a file from a jar file.
  * Files are cached in memory.
- * @param path The name of the file to read.
- * For example: io/schinzel/samples/component/pages/user_account/intro_text
+ * @param caller The class that is calling this class.
  */
-class JarFileReader(private val path: String) : IFileReader {
-
-    /**
-     * @param caller The class that is calling this class.
-     */
-    constructor(caller: Any) : this(FileReaderUtil.pathFromProjectRootToCaller(caller))
+class JarFileReader(private val caller: Any) : IFileReader {
 
     companion object {
         private val cache = mutableMapOf<String, String>()
     }
 
     override fun getFileContent(fileName: String): String {
+        // For example: io/schinzel/page_elements/samples/component/pages
+        val pathFromProjectRootToCaller = FileReaderUtil.pathFromProjectRootToCaller(caller)
         // Get the full path to the file
         // For example: /io/schinzel/samples/component/pages/user_account/intro_text/template.html
-        val pathToFile = "/$path/$fileName"
+        val pathToFile = "/$pathFromProjectRootToCaller/$fileName"
         // Return cached content if exists, otherwise read file and cache it
         return cache.getOrPut(pathToFile) {
             // Read the file
